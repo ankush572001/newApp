fcc05e5573e9b66f250056f00eafe52a1445d415
akush572001 <ankush.raut@appxbuild.com>
2022-09-07 12:26:44 +0530

    first commit

---
+++ components/game/NumberContainer.js
@@ -0,0 +1,33 @@
+import {View,Text,StyleSheet,Dimensions} from 'react-native';
+import Colors from '../../constants/colors';
+function NumberContainer({children}){
+    return (
+        <View style={styles.container}>
+            <Text style={styles.numbetText}>
+            {children}
+            </Text>
+        </View>
+    );
+}
+
+export default NumberContainer;
+
+const deviceWidth=Dimensions.get('window').width;
+
+const styles = StyleSheet.create({
+    container:{
+      borderWidth:4,
+      borderColor:Colors.accent500,
+      padding :deviceWidth <380 ?12:24,
+      margin:deviceWidth <380 ?12:24,
+      borderRadius:8,
+      justifyContent:'center',
+      alignItems:'center',
+    },
+    numbetText:{
+      color:Colors.accent500,
+      fontSize:deviceWidth <380 ?28:36,
+    //   fontWeight:'bold'
+    fontFamily:'open-sans-bold',
+    }
+});
